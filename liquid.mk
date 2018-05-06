bf0f3e79ae2a06eb551e449ddc2117bf7935af7e
Aleksander <johnnylittleplanet@gmail.com>
2018-05-06 13:01:15 +0200

    g3: initial liquid g3 [2/2]

--- liquid.mk
+++ liquid.mk
@@ -20,9 +20,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d850 device
 $(call inherit-product, device/lge/d850/device.mk)
 
+# Inherit some common LiquidRemix stuff.
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d850
-PRODUCT_NAME := full_d850
+PRODUCT_NAME := liquid_d850
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D850
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_att_us-user 4.4.2 KVT49L.D85010d D85010d.1403173065 release-keys"
