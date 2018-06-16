cb916d85eab269053520534446932b9debc15e53
Aleksander <johnnylittleplanet@gmail.com>
2018-06-16 16:07:08 +0200

    Revert "d850: Update for early-mounted system partition"
    This reverts commit 88b64b43d8c6e2f46f8ee4cec7208eb326355be8.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/lge/d850/rootdir/etc/fstab.recovery
+TARGET_RECOVERY_FSTAB := device/lge/d850/rootdir/etc/fstab.g3
 
 # NFC
 BOARD_NFC_CHIPSET := pn547
